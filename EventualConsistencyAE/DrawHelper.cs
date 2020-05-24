280bbfcc5724bc4766927bb0911efd951645c4de
Suchyyy <norbertwychowski@gmail.com>
2020-05-24 18:19:01 +0200

    fixed circle positions

--- EventualConsistencyAE/DrawHelper.cs
+++ EventualConsistencyAE/DrawHelper.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using EventualConsistencyAE.Web;
-using Service.Model;
+
+// ReSharper disable AccessToStaticMemberViaDerivedType
 
 namespace EventualConsistencyAE
 {
@@ -27,23 +26,6 @@ namespace EventualConsistencyAE
             {
                 var server = servers[i];
 
-                var text = new Label {Content = server.Port};
-                Canvas.SetTop(text, -Math.Cos(i * angle) * centerY * 0.9 + centerY);
-                Canvas.SetLeft(text, Math.Sin(i * angle) * centerX * 0.9 + centerX);
-                canvas.Children.Add(text);
-
-                var circle = new Ellipse
-                {
-                    Width = 50,
-                    Height = 50,
-                    StrokeThickness = 1,
-                    Stroke = Brushes.Black,
-                    Fill = Brushes.Transparent
-                };
-                Canvas.SetTop(circle, -Math.Cos(i * angle) * centerY * 0.9 + centerY);
-                Canvas.SetLeft(circle, Math.Sin(i * angle) * centerX * 0.9 + centerX);
-                canvas.Children.Add(circle);
-
                 for (var j = 0; j < servers.Count; j++)
                 {
                     if (i == j || servers[j].Service.Clients.All(client => client.Port != server.Port)) continue;
@@ -55,25 +37,40 @@ namespace EventualConsistencyAE
                         X2 = Math.Sin(j * angle) * centerX * 0.9 + centerX,
                         Y2 = -Math.Cos(j * angle) * centerY * 0.9 + centerY,
                         Stroke = Brushes.Black,
-                        StrokeThickness = 1
+                        StrokeThickness = 1,
                     };
+
+                    Canvas.SetZIndex(line, 1);
 
                     canvas.Children.Add(line);
                 }
+
+                var circle = new Ellipse
+                {
+                    Width = 50,
+                    Height = 50,
+                    StrokeThickness = 1,
+                    Stroke = Brushes.Black,
+                    Fill = Brushes.White
+                };
+                Canvas.SetTop(circle, -Math.Cos(i * angle) * centerY * 0.9 + centerY - 25);
+                Canvas.SetLeft(circle, Math.Sin(i * angle) * centerX * 0.9 + centerX - 25);
+                Canvas.SetZIndex(circle, 2);
+                canvas.Children.Add(circle);
+
+                var text = new Label
+                {
+                    Content = server.Port,
+                    Width = 50,
+                    Height = 50,
+                    HorizontalContentAlignment = HorizontalAlignment.Center,
+                    VerticalContentAlignment = VerticalAlignment.Center
+                };
+                Canvas.SetTop(text, -Math.Cos(i * angle) * centerY * 0.9 + centerY - 25);
+                Canvas.SetLeft(text, Math.Sin(i * angle) * centerX * 0.9 + centerX - 25);
+                Canvas.SetZIndex(text, 3);
+                canvas.Children.Add(text);
             }
-        }
-
-        private static Size GetTextSize(Label label)
-        {
-            var formattedText = new FormattedText((string) label.Content,
-                CultureInfo.CurrentCulture,
-                label.FlowDirection,
-                new Typeface(label.FontFamily, label.FontStyle, label.FontWeight, label.FontStretch),
-                label.FontSize,
-                Brushes.Black,
-                1);
-
-            return new Size(formattedText.Width, formattedText.Height);
         }
     }
 }
